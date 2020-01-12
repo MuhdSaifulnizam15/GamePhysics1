d19c2276657c88a005d63f2ea1d8b094b960d4ee
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-01-13 00:58:16 +0800

    Added new parameter for Myrectangle class

---
+++ Game.cpp
@@ -0,0 +1,39 @@
+#include "Game.h"
+
+void Game::initVariables()
+{
+
+}
+
+void Game::initWindow()
+{
+
+}
+
+void Game::initFonts()
+{
+
+}
+
+void Game::initText()
+{
+
+}
+
+void Game::initEnemies()
+{
+
+}
+
+Game::Game()
+{
+	this->initVariables();
+	this->initWindow();
+	this->initFonts();
+	this->initText();
+}
+
+Game::~Game()
+{
+
+}
---
+++ Game.h
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<ctime>
+#include<sstream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <Box2D/Box2D.h>
+
+/* Class that acts as the game engine. */
+class Game
+{
+	//Variables, Window
+	sf::RenderWindow* window;
+	sf::VideoMode videoMode;
+	sf::Event ev;
+
+	//Create gravity and world, then assign gravity to the world
+};
